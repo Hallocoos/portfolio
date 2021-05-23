91a39b3a7b5e18bfa9057a66c5c0b20779a1b31e
Hallocoos <45897219+Hallocoos@users.noreply.github.com>
2021-05-23 15:24:12 +0200

    added firbaseConfig environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA8m1C4riSfcy-Pw8qgxTjiBV_yKarSfL4",
+    authDomain: "portfolio-4577e.firebaseapp.com",
+    projectId: "portfolio-4577e",
+    storageBucket: "portfolio-4577e.appspot.com",
+    messagingSenderId: "77927338169",
+    appId: "1:77927338169:web:38f666972e10a4b63a0b68",
+    measurementId: "G-JVEST2KV6L"
+  }
 };
 
 /*
