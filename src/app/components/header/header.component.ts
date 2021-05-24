ae6b8d0beb336da5df0b82a5c3e18f69c8940e92
Hallocoos <45897219+Hallocoos@users.noreply.github.com>
2021-05-24 21:06:40 +0200

    updated CV link and removed --prod arg from package.json

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  resumeDownloadLink = 'https://firebasestorage.googleapis.com/v0/b/portfolio-4577e.appspot.com/o/CV.pdf?alt=media&token=8c4c2e7c-8b76-4f43-9e15-d7c582782082'; // Add download link here
+  resumeDownloadLink = 'https://firebasestorage.googleapis.com/v0/b/portfolio-4577e.appspot.com/o/Henricus%20Wouter%20de%20Vos%20-%20CV.pdf?alt=media&token=dbd0d14f-15d0-42cd-9f03-971c2026075c'; // Add download link here
   githubLink = 'https://github.com/Hallocoos?tab=repositories'; // Add github link here
   linkedinLink = 'https://www.linkedin.com/in/henricus-wouter-de-vos-112257193/'; // You guessed it!! LinkedIn link here
 
